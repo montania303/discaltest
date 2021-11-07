c09f079a4b9cb1164727e9762646c63de46ca283
Moises Montania <moises@datapar.com.py>
2021-11-07 14:54:23 -0300

    Actualización de Repositorio

--- Backend/discaltest/settings.py
+++ Backend/discaltest/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whitenoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'discaltest.urls'
@@ -77,13 +76,8 @@ WSGI_APPLICATION = 'discaltest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-import dj_database_url
-from decouple import config
-
 DATABASES = {
-    "default": dj_database_url.config(default=config('DATABASE_URL'))
-    
-    #env.db("DATABASE_URL", default="postgres:///discaltest") ,
+    "default": env.db("DATABASE_URL", default="postgres:///discaltest") ,
 }
 
 
@@ -123,14 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.Path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    s.Path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
